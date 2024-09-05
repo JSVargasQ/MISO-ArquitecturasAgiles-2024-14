0be64aaad2671d63be1f6e1b1a8678c826eb9a09
publio.diaz <p.diazp@uniandes.edu.co>
2024-09-04 19:17:35 -0500

    Avances creacion microservicio

--- ms_user_management/__init__.py
+++ ms_user_management/__init__.py
@@ -4,7 +4,7 @@ def create_app(config_name):
     app = Flask(__name__)
     
     # SQL Alchemy ORM
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///call_handling.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///abcall.db'
     app.config['SQLALCHEMY_TRACK_MODIFICACIONS'] = False
     
     # Exceptions
