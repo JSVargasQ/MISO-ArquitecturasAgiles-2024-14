135024429ed4a4c9f32e03c66348bda2b48816da
publio.diaz <publio.diaz@urt.gov.co>
2024-09-20 16:10:22 -0500

    Add verification hash pqr

--- ms_pqrs_management/pqrs.py
+++ ms_pqrs_management/pqrs.py
@@ -1,6 +1,8 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 import requests
 from faker import Faker
+import hashlib
+import json
 
 app = Flask(__name__)
 faker = Faker()
@@ -28,7 +30,7 @@ def get_pqrs():
 
 @app.get('/pqr')
 def get_pqr():
-    return {
+    json_data={
         "user_id": faker.random_int(min=1000, max=9999),
         "type": faker.random_element(elements=["Petición", "Queja", "Reclamo"]),
         "description": faker.text(max_nb_chars=100),
@@ -51,3 +53,35 @@ def get_pqr():
             "status": faker.random_element(elements=["Pendiente", "Resuelto", "En Proceso"]),
             "submitted_at": faker.iso8601()
         }
+    # Convertir el JSON a una cadena ordenada para asegurar que la estructura sea siempre la misma
+    json_str = json.dumps(json_data, sort_keys=True)
+    print(json_str)
+    # Generar el hash del JSON
+    json_hash = generate_hash_from_json(json_str)
+    response = {
+        "data": json_data,
+        "hash": json_hash
+    }
+    return  jsonify(response), 200
+
+def generate_hash_from_json(json_data):
+    # Convertir el JSON a una cadena ordenada para asegurar que la estructura sea siempre la misma
+    json_str = json.dumps(json_data, sort_keys=True)
+    
+    # Crear un hash SHA-256 del string JSON
+    hash_object = hashlib.sha256(json_str.encode('utf-8'))
+    
+    # Convertir el hash en un string hexadecimal
+    hash_hex = hash_object.hexdigest()
+    
+    return hash_hex
+
+@app.route('/hash_pqr', methods=['POST'])
+def get_hash_pqr():
+    json_data = request.json  # Obtiene el JSON del cuerpo de la solicitud
+    json_str = json.dumps(json_data, sort_keys=True)
+    json_hash = generate_hash_from_json(json_str)
+    response = {
+        "hash": json_hash
+    }
+    return jsonify(response), 20
